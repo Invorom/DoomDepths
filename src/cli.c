053931264cd517ca28504e0cf191833eed928356
romain.nerot <romain.nerot@veolia.com>
2023-10-02 16:18:48 +0200

    Lighter function

--- src/cli.c
+++ src/cli.c
@@ -1,17 +1,6 @@
 #include "cli.h"
 #include "monster.h"
 
-// ANSI escape codes for text colors
-// Different shades of red
-#define RED "\x1b[31m"
-#define RED_2 "\x1b[38;5;196m"
-#define RED_3 "\x1b[38;5;160m"
-#define RED_4 "\x1b[38;5;124m"
-#define RED_5 "\x1b[38;5;88m"
-#define RED_6 "\x1b[38;5;52m"
-
-#define RESET "\x1b[0m"
-
 void clear_screen()
 {
     printf("\e[1;1H\e[2J");
@@ -59,15 +48,7 @@ void main_menu()
     do
     {
         clear_screen();
-        display_hero();
-
-        // Print "DOOMDEPTHS" in ASCII art with colors
-        printf(RED " _____    ____    ____   __  __  _____   ______  _____  _______  _    _   _____ \n");
-        printf(RED_2 "|  __ \\  / __ \\  / __ \\ |  \\/  ||  __ \\ |  ____||  __ \\|__   __|| |  | | / ____|\n");
-        printf(RED_3 "| |  | || |  | || |  | || \\  / || |  | || |__   | |__) |  | |   | |__| || (___  \n");
-        printf(RED_4 "| |  | || |  | || |  | || |\\/| || |  | ||  __|  |  ___/   | |   |  __  | \\___ \\ \n");
-        printf(RED_5 "| |__| || |__| || |__| || |  | || |__| || |____ | |       | |   | |  | | ____) |\n");
-        printf(RED_6 "|_____/  \\____/  \\____/ |_|  |_||_____/ |______||_|       |_|   |_|  |_||_____/ \n" RESET);
+        display_menu_design();
 
         printf("\n 1. Start Game\n 2. Quit\n\n");
         printf("Enter your choice: ");
